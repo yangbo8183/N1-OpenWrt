882d8f160739c631e3d498edbf22154686e9e3b7
yangbo8183 <76947807+yangbo8183@users.noreply.github.com>
2024-04-26 18:52:05 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -9,3 +9,4 @@ rm -rf feeds/luci/applications/luci-app-passwall
 # Add packages
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone --depth=1 https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
