54ed073c22bcb7f9683e482cb72465f705ca869d
yangbo8183 <76947807+yangbo8183@users.noreply.github.com>
2025-08-11 20:42:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
-git clone https://github.com/openwrt/luci --depth=1 clone/luci
+git clone https://pkgs.org/download/luci-i18n-ttyd-zh-cn --depth=1 clone/luci-i18n-ttyd-zh-cn
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
